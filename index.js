331c2a4d5603db98a4fc1a87a6e70e2ad0a21450
Raphael Braga <rbragamouraa@gmail.com>
2022-09-01 19:53:20 -0300

    Implementação do Mongodb

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from 'express'
 import cors from 'cors'
+import {MongoClient} from 'mongodb'
+import dotenv from "dotenv";
+
+
+dotenv.config()
 
 const server = express()
 server.use(cors())
 server.use(express.json())
 
+const MongoCliente= new MongoClient(process.env.MONGO_URI)
+
+console.log(MongoCliente)
+
+const names=[]
+
 
 server.get('/participants',(req,res)=>{
-    res.send('OK')
+    res.send(names)
 })
 server.post('/participants',(req,res)=>{
-    res.send('ok')
+    const name = req.body
+    names.push(name)
+
+    res.send(names)
 })
 
 server.post('/messages',(req,res)=>{
